8ac65f1510ffde6e677eb3716b2c5d0d1e0875b4
Tomasz Wlostowski <tomasz.wlostowski@cern.ch>
2011-08-23 16:38:55 +0200

    WIP

--- modules/wr_endpoint/build_wb.sh
+++ modules/wr_endpoint/build_wb.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 mkdir -p doc
-~/wbgen2/wishbone-gen/wbgen2 -D ./doc/wrsw_endpoint.html -p ep_registers_pkg.vhd -H record -V ep_wishbone_controller.vhd -C ../../../software/include/hw/endpoint_regs.h --cstyle defines --lang vhdl -K ../../sim/endpoint_regs.v ep_wishbone_controller.wb
-wbgen2 -D ./doc/wrsw_endpoint_mdio.html -V ep_pcs_tbi_mdio_wb.vhd -C ../../../software/include/hw/endpoint_mdio.h --cstyle defines --lang vhdl -K ../../sim/endpoint_mdio.v pcs_regs.wb
+~/wbgen2/wishbone-gen/wbgen2 -D ./doc/wrsw_endpoint.html -p ep_registers_pkg.vhd -H record -V ep_wishbone_controller.vhd  --cstyle defines --lang vhdl -K ../../sim/endpoint_regs.v ep_wishbone_controller.wb
+wbgen2 -D ./doc/wrsw_endpoint_mdio.html -V ep_pcs_tbi_mdio_wb.vhd --cstyle defines --lang vhdl -K ../../sim/endpoint_mdio.v pcs_regs.wb
