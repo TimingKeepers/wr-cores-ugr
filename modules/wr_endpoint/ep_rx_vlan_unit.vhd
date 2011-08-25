e215393a84b2dea9ae9548b73bab44ebaf3219e8
Tomasz Wlostowski <tomasz.wlostowski@cern.ch>
2011-08-25 17:59:53 +0200

    WR Endpoint: working RX VLAN unit

--- modules/wr_endpoint/ep_rx_vlan_unit.vhd
+++ modules/wr_endpoint/ep_rx_vlan_unit.vhd
@@ -3,7 +3,6 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 library work;
-use work.gencores_pkg.all;              -- for gc_crc_gen
 use work.endpoint_private_pkg.all;
 use work.ep_wbgen2_pkg.all;
 
@@ -19,8 +18,9 @@ entity ep_rx_vlan_unit is
        src_fab_o  : out t_ep_internal_fabric;
        src_dreq_i : in  std_logic;
 
-       tclass_o       : out std_logic_vector(2 downto 0);
-       tclass_valid_o : out std_logic;
+       tclass_o   : out std_logic_vector(2 downto 0);
+       vid_o      : out std_logic_vector(11 downto 0);
+       tag_done_o : out std_logic;
 
        rmon_o : inout t_rmon_triggers;
        regs_b : inout t_ep_registers
@@ -32,7 +32,7 @@ architecture behavioral of ep_rx_vlan_unit is
 
   
   type t_tag_type is (NONE, PRIO, VLAN, NULL_VLAN);
-  type t_state is (WAIT_FRAME, DATA, DISCARD_FRAME, INSERT_TAG);
+  type t_state is (WAIT_FRAME, DATA, FLUSH_STALL, DISCARD_FRAME, INSERT_TAG, END_FRAME);
 
   signal dreq_mask  : std_logic;
   signal hdr_offset : std_logic_vector(11 downto 0);
@@ -47,8 +47,10 @@ architecture behavioral of ep_rx_vlan_unit is
   signal at_tpid          : std_logic;
   signal is_tagged        : std_logic;
   signal stored_ethertype : std_logic_vector(15 downto 0);
+  signal stored_fab       : t_ep_internal_fabric;
 
-  signal prio_int : std_logic_vector(2 downto 0);
+  signal prio_int     : std_logic_vector(2 downto 0);
+  signal force_dvalid : std_logic;
 
 
   procedure f_vlan_decision
@@ -92,6 +94,7 @@ architecture behavioral of ep_rx_vlan_unit is
         end case;
 
       when c_QMODE_PORT_UNQUALIFIED =>
+        
         case tag_type is
           when NONE =>
             admit    := '1';
@@ -105,6 +108,26 @@ architecture behavioral of ep_rx_vlan_unit is
           when NULL_VLAN =>
             admit := '0';
         end case;
+
+      when c_QMODE_PORT_VLAN_DISABLED =>
+        case tag_type is
+          when NONE =>
+            admit          := '1';
+            use_pvid       := '0';
+            use_fixed_prio := '1';
+          when PRIO=>
+            admit          := '1';
+            use_pvid       := '0';
+            use_fixed_prio := '0';
+          when VLAN=>
+            admit          := '1';
+            use_pvid       := '0';
+            use_fixed_prio := '0';
+          when NULL_VLAN =>
+            admit          := '1';
+            use_pvid       := '0';
+            use_fixed_prio := '0';
+        end case;
       when others => null;
     end case;
   end procedure;
@@ -112,18 +135,16 @@ architecture behavioral of ep_rx_vlan_unit is
   
   
   
-  
-  
 begin  -- behavioral
 
-  at_ethertype <= hdr_offset(5) and snk_fab_i.dvalid and src_dreq_i;
-  at_tpid      <= hdr_offset(6) and snk_fab_i.dvalid and is_tagged and src_dreq_i;
-  at_vid       <= hdr_offset(7) and snk_fab_i.dvalid and is_tagged and src_dreq_i;
+  at_ethertype <= hdr_offset(6) and snk_fab_i.dvalid;
+  at_tpid      <= hdr_offset(7) and snk_fab_i.dvalid and is_tagged;
+  at_vid       <= hdr_offset(8) and snk_fab_i.dvalid and is_tagged;
 
   regs_b <= c_ep_registers_init_value;
 
 
-  snk_dreq_o <= src_dreq_i and dreq_mask;
+  snk_dreq_o <= src_dreq_i and dreq_mask and not at_ethertype;
 
 
   p_decode_tag_type : process(snk_fab_i, is_tagged)
@@ -141,6 +162,10 @@ begin  -- behavioral
 
   p_tag_untag : process(clk_sys_i)
     variable admit, use_pvid, use_fixed_prio : std_logic;
+    variable v_dreq_mask                     : std_logic;
+    variable v_stored_fab                    : t_ep_internal_fabric;
+    variable v_src_fab                       : t_ep_internal_fabric;
+    variable v_next_state                    : t_state;
   begin
     if rising_edge(clk_sys_i) then
       if rst_n_i = '0' or regs_b.ecr_rx_en_o = '0' then
@@ -148,6 +173,11 @@ begin  -- behavioral
         hdr_offset(0)                        <= '1';
         state                                <= WAIT_FRAME;
         dreq_mask                            <= '0';
+
+        src_fab_o.eof    <= '0';
+        src_fab_o.error  <= '0';
+        src_fab_o.dvalid <= '0';
+        
       else
 
         if(snk_fab_i.error = '1') then
@@ -159,6 +189,8 @@ begin  -- behavioral
               dreq_mask       <= '1';
               src_fab_o.eof   <= '0';
               src_fab_o.error <= '0';
+              is_tagged       <= '0';
+              prio_int        <= regs_b.vcr0_prio_val_o;
 
               if(snk_fab_i.sof = '1') then
                 hdr_offset(hdr_offset'left downto 1) <= (others => '0');
@@ -168,38 +200,51 @@ begin  -- behavioral
 
             when DATA =>
 
-              dreq_mask <= '1';
+              v_dreq_mask     := '1';
+              v_src_fab.eof   := '0';
+              v_src_fab.error := '0';
+              v_next_state    := DATA;
 
-              -- pass the data through the pipeline
-              src_fab_o.data   <= snk_fab_i.data;
-              src_fab_o.dvalid <= snk_fab_i.dvalid;
+              if (snk_fab_i.dvalid = '1' and src_dreq_i = '0') then
+                v_stored_fab.bytesel := snk_fab_i.bytesel;
+                v_stored_fab.data    := snk_fab_i.data;
+                v_stored_fab.dvalid  := '1';
+                v_next_state         := FLUSH_STALL;
+              else
+                v_src_fab.data    := snk_fab_i.data;
+                v_src_fab.dvalid  := src_dreq_i and snk_fab_i.dvalid;
+                v_src_fab.bytesel := snk_fab_i.bytesel;
+              end if;
 
               if(at_ethertype = '1') then
+                stored_ethertype <= snk_fab_i.data;
+
                 if(snk_fab_i.data = x"8100") then  -- got a 802.1q tagged frame
                   is_tagged <= '1';
                 else
-                  is_tagged <= '0';
-                  -- if the packet has to be VLAN-tagged on ingress, do it as
-                  -- early as possible (i.e. right after identifying the Ethertype)
-                  -- to avoid increasing latency
-                  if(regs_b.vcr0_qmode_o = c_QMODE_PORT_ACCESS) then
-                    -- fix to Jose's table: UNQUALIFIED ports don't tag untagged
-                    -- frames with PVID
-                    src_fab_o.dvalid <= '0';
-                    src_fab_o.data   <= x"8100";
-                    prio_int         <= regs_b.vcr0_prio_val_o;
-                    state            <= INSERT_TAG;
-                    dreq_mask        <= '0';
-                  end if;
-                end if;
+                  if (regs_b.vcr0_qmode_o = c_QMODE_PORT_ACCESS or regs_b.vcr0_qmode_o = c_QMODE_PORT_UNQUALIFIED) then
+                    prio_int <= regs_b.vcr0_prio_val_o;
 
-                -- remember the Ethertype, as it is needed for producing an
-                -- 802.1q header
-                stored_ethertype <= snk_fab_i.data;
+                    v_src_fab.dvalid := '0';
+                    v_next_state     := INSERT_TAG;
+                    v_dreq_mask      := '0';
+                  elsif (regs_b.vcr0_qmode_o = c_QMODE_PORT_TRUNK) then
+                    v_src_fab.dvalid := '0';
+                    v_next_state     := DISCARD_FRAME;
+                  end if;
+                  is_tagged <= '0';
+                end if;
               end if;
 
-              -- we are at the VID field in the header. Most of the VLAN logic lives
-              -- here :)
+              if(snk_fab_i.eof = '1') then
+                if(src_dreq_i = '1') then
+                  v_src_fab.eof := '1';
+                  v_next_state  := WAIT_FRAME;
+                else
+                  v_next_state := END_FRAME;
+                end if;
+              end if;
+
               if(at_vid = '1') then
 
                 -- decide what to do with the frame, basing on the port mode
@@ -207,15 +252,16 @@ begin  -- behavioral
                 -- or not.
                 f_vlan_decision(comb_tag_type, regs_b.vcr0_qmode_o, admit, use_pvid, use_fixed_prio);
 
-                if(admit = '0') then    -- oops...
-                  state <= DISCARD_FRAME;
-                end if;
 
                 -- assign the VID
-                if(use_pvid = '1')then
-                  src_fab_o.data(11 downto 0) <= regs_b.vcr0_pvid_o;
+                if(admit = '0') then    -- oops...
+                  v_next_state := DISCARD_FRAME;
                 end if;
 
+                if(use_pvid = '1')then
+                  v_stored_fab.data(11 downto 0) := regs_b.vcr0_pvid_o;
+                  v_src_fab.data(11 downto 0)    := regs_b.vcr0_pvid_o;
+                end if;
 
                 -- assign the priority 
                 if(regs_b.vcr0_fix_prio_o = '1' or use_fixed_prio = '1') then
@@ -228,15 +274,44 @@ begin  -- behavioral
                 end if;
               end if;
 
-              if(snk_fab_i.eof = '1') then
-                state         <= WAIT_FRAME;
-                src_fab_o.eof <= '1';
-              end if;
-
               if(snk_fab_i.dvalid = '1') then
                 hdr_offset <= hdr_offset(hdr_offset'left-1 downto 0) & '0';
               end if;
 
+              src_fab_o.eof     <= v_src_fab.eof;
+              src_fab_o.dvalid  <= v_src_fab.dvalid;
+              src_fab_o.error   <= v_src_fab.error;
+              src_fab_o.data    <= v_src_fab.data;
+              src_fab_o.bytesel <= v_src_fab.bytesel;
+
+              dreq_mask  <= v_dreq_mask;
+              stored_fab <= v_stored_fab;
+              state      <= v_next_state;
+              
+            when END_FRAME =>
+              if(src_dreq_i = '1')then
+                src_fab_o.eof <= '1';
+                src_fab_o.dvalid <= '0';
+                state         <= WAIT_FRAME;
+              end if;
+              
+            when DISCARD_FRAME =>
+              if(src_dreq_i = '1') then
+                src_fab_o.error <= '1';
+                src_fab_o.dvalid <= '0';
+                state           <= WAIT_FRAME;
+              end if;
+
+            when FLUSH_STALL =>
+              if(src_dreq_i = '1')then
+                src_fab_o.data   <= stored_fab.data;
+                src_fab_o.dvalid <= stored_fab.dvalid;
+                src_fab_o.bytesel <= stored_fab.bytesel;
+                state            <= DATA;
+              else
+                src_fab_o.DATA   <= (others => 'X');
+                src_fab_o.dvalid <= '0';
+              end if;
 
             when INSERT_TAG =>
               src_fab_o.dvalid <= '0';
@@ -244,24 +319,25 @@ begin  -- behavioral
               if(src_dreq_i = '1') then
 -- we are at 7th word from the beginning of the frame, but the sink reception
 -- is disabled, so we can insert the original ethertype as the TPID
-                if(hdr_offset(6) = '1') then
+                if(hdr_offset(7) = '1') then
+                  src_fab_o.data   <= x"8100";
+                  src_fab_o.dvalid <= '1';
+                end if;
+
+                if(hdr_offset(8) = '1') then
                   src_fab_o.data   <= stored_ethertype;
                   src_fab_o.dvalid <= '1';
                 end if;
 
-                if(hdr_offset(7) = '1') then
+                if(hdr_offset(9) = '1') then
                   src_fab_o.data   <= regs_b.vcr0_prio_val_o & '0' & regs_b.vcr0_pvid_o;
                   state            <= DATA;
                   src_fab_o.dvalid <= '1';
                 end if;
 
                 hdr_offset <= hdr_offset(hdr_offset'left-1 downto 0) & '0';
-              end if;
-
-            when DISCARD_FRAME =>
-              if(src_dreq_i = '1') then
-                src_fab_o.error <= '1';
-                state           <= WAIT_FRAME;
+              else
+                src_fab_o.dvalid <= '0';
               end if;
           end case;
         end if;
@@ -270,18 +346,18 @@ begin  -- behavioral
   end process;
 
 
-  -- Process: p_map_prio_to_tc
-  -- Maps the PCP value from the 802.1q header into a traffic class for further
-  -- processing. The mapping table is stored in TCAR register.
+-- Process: p_map_prio_to_tc
+-- Maps the PCP value from the 802.1q header into a traffic class for further
+-- processing. The mapping table is stored in TCAR register.
   p_map_prio_to_tc : process(clk_sys_i)
   begin
     if rising_edge(clk_sys_i) then
       if(rst_n_i = '0' or regs_b.ecr_rx_en_o = '0' or snk_fab_i.sof = '1')then
-        tclass_valid_o <= '0';
-      elsif(hdr_offset(8) = '1') then
+        tag_done_o <= '0';
+      elsif(hdr_offset(9) = '1') then
         -- we're already after the headers, so prio_int is
         -- certainly valid
-        tclass_valid_o <= '1';
+        tag_done_o <= '1';
         case prio_int is
           when "000"  => tclass_o <= regs_b.tcar_pcp_map_o(2 downto 0);
           when "001"  => tclass_o <= regs_b.tcar_pcp_map_o(5 downto 3);
@@ -297,7 +373,7 @@ begin  -- behavioral
     end if;
   end process;
 
-  src_fab_o.sof    <= regs_b.ecr_rx_en_o and snk_fab_i.sof;
+  src_fab_o.sof <= regs_b.ecr_rx_en_o and snk_fab_i.sof;
 
 end behavioral;
 
