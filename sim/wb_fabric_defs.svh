44adb8465af4a8cc67b32d6c6d0e62d60a81f7b6
Tomasz Wlostowski <tomasz.wlostowski@cern.ch>
2011-10-24 23:42:16 +0200

    simulation_models: WB Fabric definitions: synced OOB types with VHDL package

--- sim/wb_fabric_defs.svh
+++ sim/wb_fabric_defs.svh
@@ -6,8 +6,8 @@ const bit [2:0] WRF_DATA = 3'b000;
 const bit [2:0] WRF_OOB = 3'b010;
 const bit [2:0] WRF_USER = 3'b110;
 
-const bit [3:0] WRF_OOB_TX_FID = 4'b0000;
-const bit [3:0] WRF_OOB_RX_TIMESTAMP = 4'b0001;
+const bit [3:0] WRF_OOB_TX_FID = 4'b0001;
+const bit [3:0] WRF_OOB_RX_TIMESTAMP = 4'b0000;
 
 `endif //  `ifndef __WB_FABRIC_DEFS_SVH
 
