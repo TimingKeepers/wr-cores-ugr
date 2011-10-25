5131662818187fafc0e2cda1a03743ef4cd5c956
Tomasz Wlostowski <tomasz.wlostowski@cern.ch>
2011-10-25 14:57:31 +0200

    wrc_core:  added wrc_syscon

---
+++ modules/wrc_core/build_wb.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+mkdir -p doc
+wbgen2 -D ./doc/wrc_syscon.html -p wrc_syscon_pkg.vhd -H record -V wrc_syscon_wb.vhd  --cstyle defines --lang vhdl -K ../../sim/wrc_syscon_regs.vh wrc_syscon_wb.wb
