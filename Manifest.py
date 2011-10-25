c4ee6035abcac221a0e20491dfdb4fd502c3f869
Tomasz Wlostowski <tomasz.wlostowski@cern.ch>
2011-10-25 23:16:49 +0200

    added missing modules to the root manifest

--- Manifest.py
+++ Manifest.py
@@ -1,9 +1,14 @@
 modules =  {"local" : 
 											[ "platform/xilinx/wr_gtp_phy",
+												"modules/wr_tbi_phy",
 												"modules/timing",
 												"modules/wr_mini_nic",
 												"modules/wr_softpll",
 												"modules/wrc_lm32",
-												"modules/wrsw_endpoint",
-												"modules/wrsw_pps_gen" ]}
+												"modules/wr_endpoint",
+												"modules/wrsw_pps_gen",
+												"modules/wrc_core" ]
+						}
+												
+										
 												
