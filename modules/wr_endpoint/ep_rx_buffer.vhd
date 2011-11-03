a71e6fd9f4eb56d1ba33703b02c09758f37602e2
Tomasz Wlostowski <tomasz.wlostowski@cern.ch>
2011-11-03 18:29:13 +0100

    wr_endpoint/ep_rx_buffer: lost OOB bugfix

--- modules/wr_endpoint/ep_rx_buffer.vhd
+++ modules/wr_endpoint/ep_rx_buffer.vhd
@@ -6,7 +6,7 @@
 -- Author     : Tomasz Włostowski
 -- Company    : CERN BE-CO-HT
 -- Created    : 2010-11-18
--- Last update: 2011-10-26
+-- Last update: 2011-10-29
 -- Platform   : FPGA-generic
 -- Standard   : VHDL'93
 -------------------------------------------------------------------------------
@@ -84,7 +84,7 @@ architecture behavioral of ep_rx_buffer is
       dout(15)          <= fab.sof;
       dout(14)          <= fab.eof;
       dout(13)          <= fab.error;
-      dout(12 downto 0) <= (others => 'X');
+      dout(12 downto 0) <= (others => '0');
       dout_valid        <= '1';
     elsif(fab.dvalid = '1') then
 
@@ -97,7 +97,7 @@ architecture behavioral of ep_rx_buffer is
       dout(15 downto 0) <= fab.data;
       dout_valid        <= '1';
     else
-      dout(17 downto 0) <= (others => 'X');
+      dout(17 downto 0) <= (others => '0');
       dout_valid        <= '0';
     end if;
 
@@ -118,14 +118,14 @@ architecture behavioral of ep_rx_buffer is
       if(din(17 downto 16) = "10") then  -- some fancy encoding is necessary here
         case cur_addr(1 downto 0) is
           when c_WRF_DATA =>
-            fab.addr <= c_WRF_OOB;
+            fab.addr <= c_WRF_OOB after 1ns;
           when c_WRF_STATUS =>
-            fab.addr <= c_WRF_DATA;
-          when others => fab.addr <= "XX";
+            fab.addr <= c_WRF_DATA after 1ns;
+          when others => fab.addr <= c_WRF_DATA after 1ns;
         end case;
 
       else
-        fab.addr <= cur_addr;
+        fab.addr <= cur_addr after 1ns;
       end if;
 
       fab.dvalid  <= not din(17) or (din(17) and not din(16));
@@ -135,14 +135,13 @@ architecture behavioral of ep_rx_buffer is
       fab.bytesel <= not din(17) and din(16);
 
     else
-      fab.bytesel <= 'X';
-      fab.addr <= (others => 'X');
-      fab.dvalid <= '0';
-      fab.sof    <= '0';
-      fab.eof    <= '0';
-      fab.error  <= '0';
-      fab.addr <= (others => 'X');
-      fab.data   <= (others => 'X');
+      fab.bytesel <= '0';
+      fab.addr    <= cur_addr after 1ns;
+      fab.dvalid  <= '0';
+      fab.sof     <= '0';
+      fab.eof     <= '0';
+      fab.error   <= '0';
+      fab.data    <= (others => '0');
     end if;
   end f_unpack_rbuf_contents;
 
@@ -164,7 +163,78 @@ architecture behavioral of ep_rx_buffer is
   signal in_prev_addr : std_logic_vector(1 downto 0);
   signal out_cur_addr : std_logic_vector(1 downto 0);
 
+  component chipscope_ila
+    port (
+      CONTROL : inout std_logic_vector(35 downto 0);
+      CLK     : in    std_logic;
+      TRIG0   : in    std_logic_vector(31 downto 0);
+      TRIG1   : in    std_logic_vector(31 downto 0);
+      TRIG2   : in    std_logic_vector(31 downto 0);
+      TRIG3   : in    std_logic_vector(31 downto 0));
+  end component;
+
+  component chipscope_icon
+    port (
+      CONTROL0 : inout std_logic_vector (35 downto 0));
+  end component;
+
+  signal CONTROL : std_logic_vector(35 downto 0);
+  signal CLK     : std_logic;
+  signal TRIG0   : std_logic_vector(31 downto 0);
+  signal TRIG1   : std_logic_vector(31 downto 0);
+  signal TRIG2   : std_logic_vector(31 downto 0);
+  signal TRIG3   : std_logic_vector(31 downto 0);
+
+  signal crappify : unsigned(10 downto 0);
+  
 begin
+  --chipscope_ila_1 : chipscope_ila
+  --  port map (
+  --    CONTROL => CONTROL,
+  --    CLK     => clk_sys_i,
+  --    TRIG0   => TRIG0,
+  --    TRIG1   => TRIG1,
+  --    TRIG2   => TRIG2,
+  --    TRIG3   => TRIG3);
+
+  --chipscope_icon_1 : chipscope_icon
+  --  port map (
+  --    CONTROL0 => CONTROL);
+
+  TRIG0(15 downto 0)  <= snk_fab_i.data;
+  trig0(16)           <= snk_fab_i.sof;
+  trig0(17)           <= snk_fab_i.eof;
+  trig0(18)           <= snk_fab_i.error;
+  trig0(19)           <= snk_fab_i.bytesel;
+  trig0(20)           <= snk_fab_i.has_rx_timestamp;
+  trig0(21)           <= snk_fab_i.dvalid;
+  trig0(24 downto 23) <= snk_fab_i.addr;
+
+  TRIG1(15 downto 0)  <= src_fab_int.data;
+  trig1(16)           <= src_fab_int.sof;
+  trig1(17)           <= src_fab_int.eof;
+  trig1(18)           <= src_fab_int.error;
+  trig1(19)           <= src_fab_int.bytesel;
+  trig1(20)           <= src_fab_int.has_rx_timestamp;
+  trig1(21)           <= src_fab_int.dvalid;
+  trig1(24 downto 23) <= src_fab_int.addr;
+
+
+  trig2(17 downto 0) <= q_in;
+  trig2(18)          <= q_in_valid;
+  trig3(17 downto 0) <= q_out;
+  trig3(18)          <= q_empty;
+  trig3(19)          <= q_wr;
+  trig3(20)          <= q_rd;
+  trig3(21)          <= q_drop;
+  trig3(22) <= q_in_valid;
+  trig3(23) <= q_out_valid;
+
+  trig3(25 downto 24) <= in_prev_addr;
+  trig3(27 downto 26) <= out_cur_addr;
+
+
+
 
   p_fifo_write : process(clk_sys_i)
   begin
@@ -191,6 +261,7 @@ begin
         case state is
           when WAIT_FRAME =>
             in_prev_addr <= c_WRF_STATUS;
+
             if(snk_fab_i.sof = '1' and q_drop = '0') then
               state <= DATA;
             end if;
@@ -206,7 +277,10 @@ begin
     end if;
   end process;
 
-  f_pack_rbuf_contents(fab_to_encode, in_prev_addr, q_in, q_in_valid);
+  p_pack_rbuf: process(fab_to_encode, in_prev_addr)
+    begin
+      f_pack_rbuf_contents(fab_to_encode, in_prev_addr, q_in, q_in_valid);
+    end process;
 
 
   p_encode_fifo_in : process(snk_fab_i, state, q_drop)
@@ -227,7 +301,7 @@ begin
     fab_to_encode <= fab_pre_encode;
   end process;
 
-  q_reset <= rst_n_i or regs_i.ecr_rx_en_o;
+  q_reset <= rst_n_i and regs_i.ecr_rx_en_o;
 
   BUF_FIFO : generic_sync_fifo
     generic map (
@@ -247,26 +321,32 @@ begin
       almost_full_o  => open,
       count_o        => q_usedw);
 
-  q_rd <= (not q_empty) and src_dreq_i;
+ 
   
-  rd_valid_gen : process(clk_sys_i, rst_n_i)
+  q_rd <=  (not q_empty) and src_dreq_i;
+
+  rd_valid_gen : process(clk_sys_i)
   begin
     if rising_edge(clk_sys_i) then
       if(rst_n_i = '0') then
         q_out_valid <= '0';
+        out_cur_addr <= c_WRF_STATUS;
       else
         q_out_valid <= q_rd;
-        if(src_fab_int.sof = '1')then
+
+        if(src_fab_int.sof = '1' or src_fab_int.eof = '1' or src_fab_int.error = '1')then
           out_cur_addr <= c_WRF_STATUS;
-        end if;
-        if(src_fab_int.dvalid = '1') then
+        else
           out_cur_addr <= src_fab_int.addr;
         end if;
       end if;
     end if;
   end process;
 
-  f_unpack_rbuf_contents(q_out, out_cur_addr, q_out_valid, src_fab_int);
+  p_unpack: process(q_out, out_cur_addr, q_out_valid)
+  begin
+    f_unpack_rbuf_contents(q_out, out_cur_addr, q_out_valid, src_fab_int);
+  end process;
 
   src_fab_o  <= src_fab_int;
   snk_dreq_o <= '1';
