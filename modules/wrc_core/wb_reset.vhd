9ec2dafb73cb7377037ba96347d3a5ac61d029ff
Tomasz Wlostowski <tomasz.wlostowski@cern.ch>
2011-11-04 02:46:01 +0100

    wrc_core/wb_reset: now has indepenent resets for the CPU and the network subsystem

--- modules/wrc_core/wb_reset.vhd
+++ modules/wrc_core/wb_reset.vhd
@@ -6,7 +6,7 @@
 -- Author     : Grzegorz Daniluk
 -- Company    : Elproma
 -- Created    : 2011-04-04
--- Last update: 2011-06-16
+-- Last update: 2011-10-29
 -- Platform   : FPGA-generics
 -- Standard   : VHDL
 -------------------------------------------------------------------------------
@@ -27,18 +27,18 @@ use ieee.std_logic_1164.all;
 
 entity wb_reset is
   port(
-    clk_i      : in  std_logic;
-    rst_n_i    : in  std_logic;
-    genrst_n_o : out std_logic;
-
-    wb_addr_i : in  std_logic_vector(1 downto 0);
-    wb_data_i : in  std_logic_vector(31 downto 0);
-    wb_data_o : out std_logic_vector(31 downto 0);
-    wb_sel_i  : in  std_logic_vector(3 downto 0);
-    wb_stb_i  : in  std_logic;
-    wb_cyc_i  : in  std_logic;
-    wb_we_i   : in  std_logic;
-    wb_ack_o  : out std_logic
+    clk_i       : in  std_logic;
+    rst_n_i     : in  std_logic;
+    rst_cpu_n_o : out std_logic;
+    rst_net_n_o : out std_logic;
+    wb_addr_i   : in  std_logic_vector(1 downto 0);
+    wb_data_i   : in  std_logic_vector(31 downto 0);
+    wb_data_o   : out std_logic_vector(31 downto 0);
+    wb_sel_i    : in  std_logic_vector(3 downto 0);
+    wb_stb_i    : in  std_logic;
+    wb_cyc_i    : in  std_logic;
+    wb_we_i     : in  std_logic;
+    wb_ack_o    : out std_logic
     );
 end wb_reset;
 
@@ -46,51 +46,52 @@ architecture behaviour of wb_reset is
 
   constant c_RST_REG : std_logic_vector(1 downto 0) := "00";
 
-  signal rst_reg : std_logic_vector(7 downto 0);
-  signal grst_n  : std_logic_vector(20 downto 0);
-  signal ack_int : std_logic;
+  signal rst_reg  : std_logic_vector(7 downto 0);
+  signal grst_net : std_logic_vector(20 downto 0);
+  signal grst_cpu : std_logic_vector(20 downto 0);
+  signal ack_int  : std_logic;
   
 begin
 
-  process(clk_i, rst_n_i)
+  process(clk_i)
   begin
-    if(rst_n_i = '0') then
-      ack_int <= '0';
-      rst_reg <= (others => '0');
-    elsif(rising_edge(clk_i)) then
-      if(wb_stb_i = '1' and wb_cyc_i = '1' and ack_int = '0') then
-        if(wb_we_i = '1') then
-          case wb_addr_i is
-            when c_RST_REG =>
-              rst_reg <= wb_data_i(7 downto 0);
-            when others =>
-          end case;
-        end if;
-        ack_int <= '1';
-      else
+    if(rising_edge(clk_i)) then
+      if(rst_n_i = '0') then
         ack_int <= '0';
+        rst_reg <= (others => '0');
+      else
+        if(wb_stb_i = '1' and wb_cyc_i = '1' and ack_int = '0') then
+          if(wb_we_i = '1') then
+            case wb_addr_i is
+              when c_RST_REG =>
+                rst_reg <= wb_data_i(7 downto 0);
+              when others =>
+            end case;
+          end if;
+          ack_int <= '1';
+        else
+          ack_int <= '0';
+        end if;
       end if;
     end if;
   end process;
 
   wb_ack_o <= ack_int;
 
-  process(clk_i, rst_n_i)
+  process(clk_i)
   begin
-    if(rst_n_i = '0') then
-      grst_n <= (others => '1');
-    elsif(rising_edge(clk_i)) then
-
-      if(rst_reg(0) = '1') then
-        grst_n(0) <= '0';
+    if(rising_edge(clk_i)) then
+      if(rst_n_i = '0') then
+        grst_net <= (others => '1');
+        grst_cpu <= (others => '1');
       else
-        grst_n(0) <= '1';
+        grst_cpu <= grst_cpu(grst_cpu'left-1 downto 0) & not rst_reg(0);
+        grst_net <= grst_net(grst_net'left-1 downto 0) & not rst_reg(1);
       end if;
-
-      grst_n(grst_n'left downto 1) <= grst_n(grst_n'left-1 downto 0);
-      
     end if;
   end process;
-  genrst_n_o <= grst_n(grst_n'left);
 
+  rst_cpu_n_o <= grst_cpu(grst_cpu'left);
+  rst_net_n_o <= grst_net(grst_cpu'left);
+  
 end behaviour;
