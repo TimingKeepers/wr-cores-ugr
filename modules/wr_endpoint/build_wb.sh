b3bfa66d665b26e3f3475d95d6836d0cb9a03bd0
Grzegorz Daniluk <g.daniluk@elproma.com.pl>
2011-11-06 00:26:22 +0100

    wr_endpoint: fixed wbgen2 path in build_wb.sh

--- modules/wr_endpoint/build_wb.sh
+++ modules/wr_endpoint/build_wb.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 mkdir -p doc
-~/wbgen2/wishbone-gen/wbgen2 -D ./doc/wrsw_endpoint.html -C endpoint_regs.h -p ep_registers_pkg.vhd -H record -V ep_wishbone_controller.vhd  --cstyle struct --lang vhdl -K ../../sim/endpoint_regs.v ep_wishbone_controller.wb
+wbgen2 -D ./doc/wrsw_endpoint.html -C endpoint_regs.h -p ep_registers_pkg.vhd -H record -V ep_wishbone_controller.vhd  --cstyle struct --lang vhdl -K ../../sim/endpoint_regs.v ep_wishbone_controller.wb
 wbgen2 -D ./doc/wrsw_endpoint_mdio.html -V ep_pcs_tbi_mdio_wb.vhd --cstyle defines --lang vhdl -K ../../sim/endpoint_mdio.v pcs_regs.wb
