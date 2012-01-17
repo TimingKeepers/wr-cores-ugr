de74b82b07829301d886473f6214758a526b40ff
Tomasz Włostowski <tomasz.wlostowski@cern.ch>
2012-01-17 15:52:16 +0100

    modules/wrc_lm32: removed legacy CPU core (now uses wr-cores)

--- Manifest.py
+++ Manifest.py
@@ -2,11 +2,11 @@ fetchto = "ip_cores"
 
 modules =  {"local" : 
 											[ "platform/xilinx/wr_gtp_phy",
+												"modules/fabric",
 												"modules/wr_tbi_phy",
 												"modules/timing",
 												"modules/wr_mini_nic",
 												"modules/wr_softpll",
-												"modules/wrc_lm32",
 												"modules/wr_endpoint",
 												"modules/wr_pps_gen",
 												"modules/wrc_core" ],
