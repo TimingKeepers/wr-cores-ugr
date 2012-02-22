74e0bc7468911132cd2a04b9323986ba91f0ad1c
Stefan Rauch <s.rauch@gsi.de>
2012-02-22 18:50:59 +0100

    Added Manifest for exploder

---
+++ top/gsi_exploder/wr_core_demo/Manifest.py
@@ -0,0 +1,8 @@
+fetchto = "../../../ip_cores"
+
+modules = {
+    "local" : "../../../modules/mini_bone",
+    "svn" : "http://svn.ohwr.org/etherbone-core/hdl/EB_SPEC_Test"
+};
+
+files = ["exploder_top.sdc", "exploder_top.vhd", "pow_reset.vhd", "spec_serial_dac.vhd"]
