593d0df72de79416b855ece92b1d77007225666d
Tomasz Włostowski <tomasz.wlostowski@cern.ch>
2012-03-16 15:20:21 +0100

    modules/timing: added pulse_gen and pulse_stamper to the Manifest

--- modules/timing/Manifest.py
+++ modules/timing/Manifest.py
@@ -1,5 +1,7 @@
 files = ["dmtd_phase_meas.vhd",
          "dmtd_with_deglitcher.vhd",
          "multi_dmtd_with_deglitcher.vhd",
-         "hpll_period_detect.vhd" ]
+         "hpll_period_detect.vhd",
+         "pulse_gen.vhd",
+         "pulse_stamper.vhd" ]
          
