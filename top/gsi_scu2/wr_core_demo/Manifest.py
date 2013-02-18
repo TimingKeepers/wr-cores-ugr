fa186f050e2f10b77d08a871d6872368f706b676
Wesley W. Terpstra <w.terpstra@gsi.de>
2013-02-18 18:31:51 +0100

    scu2: remove project. scu is now both v1&v2.

--- top/gsi_scu2/wr_core_demo/Manifest.py
+++
@@ -1,8 +0,0 @@
-fetchto = "../../../ip_cores"
-
-modules = {
-    "local" : "../../../modules/mini_bone",
-    "git" : "git://ohwr.org/hdl-core-lib/etherbone-core.git"
-};
-
-files = ["scu_top.vhd" ]
