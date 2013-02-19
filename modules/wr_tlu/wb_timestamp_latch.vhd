3369f7e3d8b360a27912e60589fdc81f12c61cc7
Wesley W. Terpstra <w.terpstra@gsi.de>
2013-02-19 15:01:09 +0100

    tlu: new regisers with TLU's timestamp (rebased change from gsi branch)

--- modules/wr_tlu/wb_timestamp_latch.vhd
+++ modules/wr_tlu/wb_timestamp_latch.vhd
@@ -13,6 +13,8 @@
 --! 0x014 n..0 channel(n) trigger edge status  (ro)
 --! 0x018 n..0 channel(n) trigger edge set pos (wo)
 --! 0x01C n..0 channel(n) trigger edge set neg (wo)
+--! 0x100 UTC				HiReg
+--! 0x104 Cycle Count	LoReg
 --! 0x400 start of FIFO addresses
 --!
 --! FIFO 0 area is at 0x400, FIFO n is at 0x400 + n*0x020
@@ -165,6 +167,10 @@ architecture behavioral of wb_timestamp_latch is
   signal tm_word0                 : t_word_array;
   signal tm_word1                 : t_word_array;
   signal tm_word2                 : t_word_array;
+  
+  signal cur_tm_word1				 : t_word;
+  signal cur_tm_word2				 : t_word;
+  
   -- fifo signals
   signal nRst_fifo                : channel;
   signal rd                       : channel;
@@ -420,7 +426,13 @@ begin  -- behavioral
                 when x"014"  => wb_slave_o.dat <= pad_4_WB(trigger_edge); wb_slave_o.ack <= '1';
                 when x"018" => null;
                 when x"01C" => null;
-                               
+                when x"100" => wb_slave_o.dat <= std_logic_vector(to_unsigned(0, 32-8)) & tm_fifo_in(0)(67 downto 60);
+										 cur_tm_word1   <= tm_fifo_in(0)(59 downto 28);
+										 cur_tm_word2   <= std_logic_vector(to_unsigned(0, 32-28)) & tm_fifo_in(0)(27 downto 0);
+										 wb_slave_o.ack <= '1';
+					 when x"104" => wb_slave_o.dat <= cur_tm_word1; wb_slave_o.ack <= '1';
+					 when x"108" => wb_slave_o.dat <= cur_tm_word2; wb_slave_o.ack <= '1';
+					 
                 when others => wb_slave_o.err <= '1';
               end case;
             end if;
