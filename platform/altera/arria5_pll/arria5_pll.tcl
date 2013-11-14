2fb2f9070226b5eaa24a16ba85c4f1bedf2e0be9
Wesley W. Terpstra <w.terpstra@gsi.de>
2013-11-14 16:05:22 +0100

    altera: support quartus 13.1
    Modification to TCL build script were needed.
    Move duplicated code to a common file where the changes apply once.

--- platform/altera/arria5_pll/arria5_pll.tcl
+++ platform/altera/arria5_pll/arria5_pll.tcl
@@ -1,15 +1,1 @@
-set files { sys_pll5 ref_pll5 dmtd_pll5 }
-
-set dir [file dirname [info script]]
-post_message "Testing for megawizard regeneration in $dir:$files"
-
-foreach i $files {
-  if {![file exists "$dir/$i.qip"] || [file mtime "$dir/$i.txt"] > [file mtime "$dir/$i.qip"]} {
-    post_message "Regenerating $i using qmegawiz"
-    file copy -force "$dir/$i.txt" "$dir/$i.vhd"
-    set sf [open "| qmegawiz -silent $dir/$i.vhd" "r"]
-    while {[gets $sf line] >= 0} { post_message "$line" }
-    close $sf
-    file mtime "$dir/$i.qip" [file mtime "$dir/$i.vhd"]
-  }
-}
+qmegawiz { sys_pll5 ref_pll5 dmtd_pll5 }
