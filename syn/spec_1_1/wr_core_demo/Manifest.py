71f3c2b6d64bd78dd2657aa5a148a83252fce4ab
Miguel Jimenez Lopez <klyone@ugr.es>
2014-01-28 09:48:03 +0100

    syn/spec_1_1/wr_core_demo/Manifest: Updated Manifest (FPGA: Virtex7)

--- syn/spec_1_1/wr_core_demo/Manifest.py
+++ syn/spec_1_1/wr_core_demo/Manifest.py
@@ -3,9 +3,9 @@ action = "synthesis"
 
 fetchto = "../../../ip_cores"
 
-syn_device = "xc6slx45t"
-syn_grade = "-3"
-syn_package = "fgg484"
+syn_device = "xc7vx690t"
+syn_grade = "-2"
+syn_package = "ffg1761"
 syn_top = "spec_top"
 syn_project = "spec_top_wrc.xise"
 
